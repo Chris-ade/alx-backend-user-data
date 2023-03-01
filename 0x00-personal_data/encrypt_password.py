ad93b3d980d83130a3c07cc19d0b0ac7f1518c90
Chris Adebiyi <joeade12054@gmail.com>
2023-03-01 09:44:00 -0800

    Task done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 """A module for encrypting passwords.
-
 """
 
 import bcrypt
@@ -11,7 +10,6 @@ def hash_password(password: str) -> bytes:
     """Hashes a password using a random salt.
 
     """
-
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
@@ -19,5 +17,4 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks is a hashed password was formed from the given password.
 
     """
-
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
